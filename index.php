a7d5bf58783905035fc761001efeae5785bfd5ac
acs <c.vauthier@codeur.online>
2021-11-04 11:45:01 +0100

    resultat

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Food app">
+    <title>Food app</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+<body>
+    <div class="container">
+        <ul class="nav">
+            <img src="images/logo.svg" alt="" srcset="" class="logo">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Process</a></li>
+        </ul>
+
+        <section class="home">
+            <h1>Beautiful food & takeaway<span>delivered</span> to your door.</h1>
+            <div class="card">
+                <img src="images/hero_image.png" alt="">
+                <div class="card__text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet, ante non placerat sollicitudin, enim felis accumsan velit, nec fringilla libero justo vitae odio.</p>
+                    <a href="#" role="button">Place an order</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="about">
+            <h1><span>The home of fresh products</span></h1>
+            <div class="card">
+                <img src="images/eat.png" alt="">
+                <div class="card__text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet, ante non placerat sollicitudin, enim felis accumsan velit, nec fringilla libero justo vitae odio.</p>
+                    <a href="#">Learn about us</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="process">
+           
+            <h1><span>How it works</span></h1>
+            <div class="mini__container">
+            <div class="mini__card">
+                <img src="images/step_01.png" alt="">
+                <h2>Adapt your menu items</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet, ante non placerat sollicitudin.</p> 
+            </div>
+            <div class="mini__card">
+                <img src="images/step_02.png" alt="">
+                <h2>Adapt your menu items</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet, ante non placerat sollicitudin.</p> 
+            </div>
+            <div class="mini__card">
+                <img src="images/step_03.png" alt="">
+                <h2>Adapt your menu items</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet, ante non placerat sollicitudin.</p> 
+            </div>
+
+
+
+
+
+            </div>
+           
+        </section>
+
+    </div>
+    
+   
+    <script src="assets/scripts/main.js"></script>
+</body>
+</html>
